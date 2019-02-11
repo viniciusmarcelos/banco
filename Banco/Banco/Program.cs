37464caed5ebb016af7ee92d3507eae8db263c5b
Vinicius <viniciusmarcelos@gmail.com>
2019-02-11 21:19:55 -0200

    Duvida

---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MyLibrary;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenu();
+
+            Console.ReadKey();
+        }
+        public static void MainMenu()
+        {
+            string[] startMenu = new string[] { "Clientes", "Contas", "Operações", "Sair" };
+            MenuLib.PrintMenu(startMenu, "SISTEMA DO BANCO");
+            string startMenuChoice = MenuLib.ReadOption(startMenu,"Digite o número correspondente à opção desejada:");
+            switch (startMenuChoice)
+            {
+                case "Clientes":
+                    ClientsMenu();
+                    break;
+                case "Contas":
+                    break;
+                case "Operações":
+                    break;
+                case "Sair":
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static void ClientsMenu()
+        {
+            string[] clientsMenu = new string[] { "Adicionar novo cliente", "Remover um cliente" , "Retornar ao menu principal"};
+            MenuLib.PrintMenu(clientsMenu, "MENU DE CLIENTES");
+            string clientsMenuChoice = MenuLib.ReadOption(clientsMenu);
+            switch (clientsMenuChoice)
+            {
+                case "Adicionar novo cliente":
+                    MainMenu();
+                    break;
+                case "Remover um cliente":
+                    break;
+                case "Retornar ao menu principal":
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
